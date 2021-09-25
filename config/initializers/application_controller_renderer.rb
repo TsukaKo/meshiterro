b87cd7802fde8be77d25c9601cc802e52cb6ef08
TsukaKo <Kodaibb3@gmail.com>
2021-09-25 17:22:05 +0000

    [finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9628acf394a5e6f1b2c89b6b909e6a520e82b73b2ed74f51041afd4ccaf54a5fe421cbf3ddfc26562c68c2bf9f7f32875dfe7424e858a04e32d0ad2f3f8d7efe'
